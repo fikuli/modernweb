f5111b62ab408cd1e06a986a54cd97b3aeb1fc80
Yilmaz <ext-bilge.yilmaz@here.com>
2020-05-15 12:42:06 +0200

    anectodes

---
+++ part1/anectodes/src/index.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+  const [counts, setCounts] = useState(Array(anecdotes.length).fill(0))
+  const [most, setMost] = useState(0)
+  const [mostIndex, setMostIndex] = useState(0)
+
+  const next = () => {
+    const x = Math.floor(Math.random() * anecdotes.length);
+    setSelected(x)
+  }
+
+  const vote = () => {
+    const copy = {...counts}
+    copy[selected] += 1;
+    setCounts(copy)
+
+    if(copy[selected]>most){
+      setMost(copy[selected])
+      setMostIndex(selected)
+    }
+  }
+
+  return (
+    <div>
+      <h1>Anectode of the day</h1>
+      <p>{props.anecdotes[selected]}</p>
+      <p>has {counts[selected]} votes</p>
+
+      <Button handleClick={vote} text="vote" />
+      <Button handleClick={next} text="next anectode" />
+
+      <h1>Anectode with most votes</h1>
+      <p>{props.anecdotes[mostIndex]}</p>
+
+    </div>
+  )
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+)
