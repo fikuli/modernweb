e381fa6769d1db54db3b83db4c9e7f14612f93c2
Yilmaz <ext-bilge.yilmaz@here.com>
2020-05-16 17:07:38 +0200

    effect hook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,20 +1,28 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Persons from './components/Persons'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
+import axios from 'axios'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }  ])
+  const [ persons, setPersons ] = useState([])
 
   const [ newSearch, setNewSearch ] = useState('')
 
   const copyPers = (eleman) =>{
     setPersons(eleman)
   }
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
+  console.log('render', persons.length, 'persons')
 
   return (
     <div>
