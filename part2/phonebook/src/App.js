2fc37c0c0a5d761bc39fa7dbadc9ab940306c4e6
Yilmaz <ext-bilge.yilmaz@here.com>
2020-05-16 12:55:58 +0200

    refactor 2

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -10,44 +10,16 @@ const App = () => {
     { name: 'Dan Abramov', number: '12-43-234345' },
     { name: 'Mary Poppendieck', number: '39-23-6423122' }  ])
 
-  const [ newName, setNewName ] = useState('')
-  const [ newNumber, setNewNumber ] = useState('')
   const [ newSearch, setNewSearch ] = useState('')
-
-
-  const addName = (event) => {
-    event.preventDefault()
-
-    function contains(person) { 
-      return person.name === newName;
-    }
-
-    if(persons.findIndex(contains) != -1) {
-      window.alert(`${newName} is already added to phonebook`);
-      return
-    }
-
-    const person = {
-      name: newName,
-      number: newNumber
-    }
-
-    setPersons(persons.concat(person))
-
-    setNewName('')
-    setNewNumber('')
-  }
 
   const personsToShow = persons.filter(person => person.name.toLowerCase().startsWith(newSearch.toLowerCase()))
 
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
-  }
   const handleSearchChange = (event) => {
     setNewSearch(event.target.value)
+  }
+
+  const copyPers = (eleman) =>{
+    setPersons(eleman)
   }
 
   return (
@@ -55,7 +27,7 @@ const App = () => {
       <h2>Phonebook</h2>
       <Filter newSearch={newSearch} handleSearchChange={handleSearchChange}/>
       <h2>Add new</h2>
-      <PersonForm addName={addName} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange}/>
+      <PersonForm persons={persons} copyPers={copyPers}/>
       <h2>Numbers</h2>
       <Persons personsToShow={personsToShow}/>
     </div>
--- part2/phonebook/src/components/PersonForm.js
+++ part2/phonebook/src/components/PersonForm.js
@@ -2,13 +2,46 @@ import React, { useState } from 'react'
 
 const PersonForm = (props) => {
 
+    const [ newName, setNewName ] = useState('')
+    const [ newNumber, setNewNumber ] = useState('')
+    const addName = (event) => {
+      event.preventDefault()
+  
+      function contains(person) { 
+        return person.name === newName;
+      }
+  
+      if(props.persons.findIndex(contains) != -1) {
+        window.alert(`${newName} is already added to phonebook`);
+        return
+      }
+  
+      const person = {
+        name: newName,
+        number: newNumber
+      }
+  
+      props.copyPers(props.persons.concat(person))
+
+      setNewName('')
+      setNewNumber('')
+    }
+
+    const handleNameChange = (event) => {
+        setNewName(event.target.value)
+      }
+      const handleNumberChange = (event) => {
+        setNewNumber(event.target.value)
+      }
+    
+
     return(
-        <form onSubmit={props.addName}>
+        <form onSubmit={addName}>
         <div>
-            name: <input value = {props.newName} onChange={props.handleNameChange}/>
+            name: <input value = {newName} onChange={handleNameChange}/>
           </div>
           <div>
-            number: <input value = {props.newNumber} onChange={props.handleNumberChange}/>
+            number: <input value = {newNumber} onChange={handleNumberChange}/>
           </div>
           <div>
             <button type="submit">add</button>
